22df1ac42e28bb3328a06428276471bb3ad10637
Joel Tang <joeltwk@gmail.com>
2023-07-09 18:16:01 +0800

    added features

--- index.js
+++ index.js
@@ -9,7 +9,33 @@ function changeImage(){
   else {
     counter = 1
   }
-  setTimeout("changeImage()", 300)
+  setTimeout("changeImage()", 600)
 }
 
+function fadeIn(){
+  let fadeElements = document.querySelectorAll(".fade")
+  fadeElements.forEach(element =>
+    element.classList.add("fade_in")
+  );
+}
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.querySelector(".clock").innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+
 window.onload = setTimeout("changeImage()", 1000)
+window.onload = fadeIn()
+window.onload = startTime()
